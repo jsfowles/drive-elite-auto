93199c03ada2809cb15d9dbb407213a83580418a
Jacob Fowles <js.fowles@gmail.com>
2022-03-28 15:57:12 -0600

    half way done with mobile

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 const Cta = () => {
   return (
-    <div className="bg-elite h-full p-8 text-white w-[450px]">
+    <div className="bg-elite p-8 text-white w-full md:max-w-[450px]">
       <h2 className="text-white pb-2.5">Let's Work Together</h2>
-      <p className="text-base pb-3.5 w-80">
+      <p className="text-base text-white pb-3.5 w-80">
         Have an idea for your ride. Schedule an appointment and let’s make it
         reality.
       </p>
       <div className="flex pb-7">
         <img src="images/footer/call.svg" alt="" />
-        <p className="pl-2 text-base font-bold">(801) 712-1070 - text only</p>
+        <p className="pl-2 text-base text-white font-bold">
+          (801) 712-1070 - text only
+        </p>
       </div>
 
       <button className="uppercase bg-white text-black font-heading px-5 py-2">
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,30 +6,37 @@ const IndexPage = () => {
   return (
     <Layout title="High End Rides / Drive Elite Auto">
       <Heading title="Elite Auto Solutions" />
-      <div className="flex justify-between">
-        <p className="w-80">
-          Get the very most from your vehicle by installing in demand features
-          for a fraction of the cost from the factory. From mobile audio, video,
-          accessories or appearance. We offer automotive solutions for just
-          about every vehicle and every budget.
-        </p>
-        <figure className="-translate-y-20 z-0">
-          <Image src="/images/home/home-hero.jpg" height={746} width={498} />
-        </figure>
-      </div>
-      <figure className="-translate-y-64 z-0 -translate-x-8">
-        <Image src="/images/home/home-truck.jpg" height={350} width={498} />
-      </figure>
-      <div className="flex justify-between -translate-y-52">
-        <CTA />
-        <figure className="-translate-y-20 z-0">
-          <Image
-            src="/images/home/home-interior.jpg"
-            height={507}
-            width={462}
-          />
-        </figure>
-      </div>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="flex flex-col items-center md:items-end">
+          <p className="pb-10">
+            Get the very most from your vehicle by installing in demand features
+            for a fraction of the cost from the factory. From mobile audio,
+            video, accessories or appearance. We offer automotive solutions for
+            just about every vehicle and every budget.
+          </p>
+          <figure className="w-full mb-6 md:mb-24">
+            <img
+              src="/images/home/home-truck.jpg"
+              className="w-full object-contain"
+            />
+          </figure>
+          <CTA />
+        </div>
+        <div className="flex flex-col items-center ">
+          <figure className="w-full mb-6">
+            <img
+              src="/images/home/home-hero.jpg"
+              className="w-full object-contain"
+            />
+          </figure>
+          <figure className="w-full mb-6 md:mb-24 flex justify-center">
+            <img
+              src="/images/home/home-interior.jpg"
+              className="w-full md:w-[450px] object-contain"
+            />
+          </figure>
+        </div>
+      </section>
     </Layout>
   );
 };
